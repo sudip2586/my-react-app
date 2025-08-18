6b542545c5d3f3d26f175868dac9331f884d9316
sudip2586 <sudip496@protonmail.com>
2025-08-18 09:24:38 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React!</h1>
+      <p>Your app is working 🎉</p>
+    </div>
+  );
+}
+
+export default App;
